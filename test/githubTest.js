40989990b502a0799a164d3d5c6bdead5e6cf6fc
Pingping Yao <Pingping.Yao@suncorp.com.au>
2016-10-12 15:37:51 +0800

    add a github test and add grunt configure file

--- test/githubTest.js
+++ test/githubTest.js
@@ -1,1 +1,27 @@
+var request = require('supertest')('https://github.com');
 
+describe('Github home page',function(){
+
+    this.timeout(10000);
+
+    //before('must be on home page',function(done){
+    //    request.get('/')
+    //        .expect(200,done);
+    //});
+
+    it('could be navigated to register page',function(done){
+        request.get('/')
+            .expect(200,done);
+    });
+
+    //it('will refuse the request if username has been taken',function(done){
+    //    request.post('/signup_check/username')
+    //        .type('form')
+    //        .send('value=lala')
+    //        .expect(404)
+    //        .end(function(err,res){
+    //            if (err) return done(err)
+    //            done();
+    //        })
+    //});
+});
